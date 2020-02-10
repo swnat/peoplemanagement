a0f899601780fdec82a2efdc53805c4ea07779f8
Alberto Fernandez <albertofer97@gmail.com>
2020-02-10 16:10:54 -0300

    fix-add-interview-test

--- front-end/src/app/interview/add-interview/add-interview.component.spec.ts
+++ front-end/src/app/interview/add-interview/add-interview.component.spec.ts
@@ -8,11 +8,22 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { ToastrService, ToastrModule } from 'ngx-toastr';
 import { HttpClientModule } from '@angular/common/http';
 import { By } from '@angular/platform-browser';
+import { User } from 'src/app/models/user';
 
+const firstUser: User = {
+  name: 'Elena',
+  lastname: 'Coronel',
+  email: 'elena@softwarenatura.com',
+  active: true,
+  rol: 'user1',
+  phoneNumber: '',
+  idUser: 4,
+};
 
 describe('AddInterviewComponent', () => {
   let component: AddInterviewComponent;
   let fixture: ComponentFixture<AddInterviewComponent>;
+  let expectedUser: User;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -24,10 +35,11 @@ describe('AddInterviewComponent', () => {
   }));
 
   beforeEach(() => {
+    expectedUser = firstUser;
     fixture = TestBed.createComponent(AddInterviewComponent);
     component = fixture.componentInstance;
     localStorage.setItem('statusCandidate', JSON.stringify('{"id":4,"name":"Interview Team"}'));
-    localStorage.setItem('currentUser', JSON.stringify('{"id":4, "rol": "user1"}'));
+    localStorage.setItem('currentUser', JSON.stringify({ "accessToken": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI0Iiwic2NvcGVzIjoidXNlcjEiLCJpYXQiOjE1NzY1ODUzMjEsImV4cCI6MTU4MTYzMzMyMX0.xBqWVdg7W8rqT7dHihiUqstQq6p3bJzhPJS7N1hE5GQR3rh6UTYxbLv1F9Zbex1_Q6mEuJR579G8uoNkHEL7WQ", "tokenType": "Bearer", "id": "4", "rol": "user1" }));
     fixture.detectChanges();
   });
 
@@ -41,7 +53,7 @@ describe('AddInterviewComponent', () => {
     expect(component.interviewForm.valid).toBeFalsy();
     //invalid form cannot click in the button save
     spyOn(component, 'onSubmit');
-    let bt = fixture.debugElement.query(By.css('#add-btn')).nativeElement;
+    let bt = fixture.debugElement.query(By.css('#save-btn')).nativeElement;
     bt.click();
     expect(component.onSubmit).toHaveBeenCalledTimes(1);
   }));
