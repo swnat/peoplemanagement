a80140735081aa38c6cdbd6d9725c2f77144bbde
José Palacios <joselupalaciosbo@gmail.com>
2020-02-13 14:42:07 -0300

    show candidate data in the card

--- front-end/src/app/candidate/list-candidate/card-candidate/card-candidate.component.ts
+++ front-end/src/app/candidate/list-candidate/card-candidate/card-candidate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Candidate } from 'src/app/models/candidate';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-candidate',
@@ -8,25 +9,42 @@ import { Candidate } from 'src/app/models/candidate';
 })
 export class CardCandidateComponent implements OnInit {
 
-  @Input() candidate: Candidate;
+  @Input() candidate: Candidate = null;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log(this.candidate);
   }
 
-  /* should return a string no longer than 15 characters */
+  // go to candidate profile page
+  onClick() {
+    let url = '/candidate/data/';
+    if (this.candidate) url += this.candidate.id;
+    this.router.navigateByUrl(url);
+  }
+
+  /* should return a string no longer than 16 characters */
   get candidateName(): string {
-    return 'Juan Pérez ...';
+    let name: string = this.candidate ? `${this.candidate.name} ${this.candidate.lastName}`.trim() : '-';
+    if (name.length > 16) name = `name.substr(0, 13)...`;
+    return name;
   }
 
   get statusChallenge(): string {
-    return 'completed';
+    const stch = this.candidate ? this.candidate.process_challenge_status : '';
+    if (stch.match(/pending/i)) return 'pending';
+    if (stch.match(/pro/i)) return 'in_process';
+    if (stch.match(/completed/i)) return 'completed';
+    return 'undefined';
   }
 
   get statusInterview(): string {
-    return 'in_process';
+    const stint = this.candidate ? this.candidate.interviewStatus : '';
+    if (stint.match(/pending/i)) return 'pending';
+    if (stint.match(/pro/i)) return 'in_process';
+    if (stint.match(/completed/i)) return 'completed';
+    return 'undefined';
   }
 
 }
