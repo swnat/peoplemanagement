f74b38351f62d7db1672f62a7745ade304903e66
José Palacios <joselupalaciosbo@gmail.com>
2020-03-09 11:59:42 -0300

    Add test for candidate card

--- front-end/src/app/candidate/list-candidate/card-candidate/card-candidate.component.spec.ts
+++ front-end/src/app/candidate/list-candidate/card-candidate/card-candidate.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { CardCandidateComponent } from './card-candidate.component';
+import { Candidate } from 'src/app/models/candidate';
 
 describe('CardCandidateComponent', () => {
   let component: CardCandidateComponent;
@@ -8,6 +9,7 @@ describe('CardCandidateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ RouterTestingModule ],
       declarations: [ CardCandidateComponent ]
     })
     .compileComponents();
@@ -16,10 +18,19 @@ describe('CardCandidateComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CardCandidateComponent);
     component = fixture.componentInstance;
+    component.candidate = { id: 1, name: 'John', lastName: 'Doe', process_challenge_status: 'PENDING',
+        interviewStatus: 'PENDING', } as Candidate;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show candidate data', () => {
+    const cardElement: HTMLElement = fixture.nativeElement;
+    expect(cardElement.textContent).toContain('John');
+    expect(cardElement.textContent).toContain(component.statusChallenge);
+    expect(cardElement.textContent).toContain(component.statusInterview);
   });
 });
