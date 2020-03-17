8fd6e018dfbe4aa07745c92c240d6bad5d2651fb
jose <familiajoselo@gmail.com>
2020-03-17 10:59:10 -0300

    add user profile link to the profile image on the card

--- front-end/src/app/shared/layout/header/header.component.ts
+++ front-end/src/app/shared/layout/header/header.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { StorageService } from 'src/app/service/storage.service';
-import { AuthenticationService } from 'src/app/service/authentication.service';
 import { Observable } from 'rxjs';
-import { UserService } from 'src/app/service/user.service';
-import { User } from 'src/app/models/user';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
-
 
 @Component({
   selector: 'app-header',
@@ -14,44 +8,13 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  /**
-   * Manejo de la Sesión
-   * @param storageService Mantiene la sesión almacenada para que sea persistente
-   * @param authenticationService Para hacer el logout
-   */
-  rol_name: String;
-  user: User = new User();
-  constructor(
-    private storageService: StorageService,
-    private authenticationService: AuthenticationService,
-    private userService: UserService) { }
+
+  constructor() { }
 
   toggled = false;
-  //
-  show = false;
+  show = false; // user card attribute
 
-
-  ngOnInit() {
-
-
-    this.userService.getUser(this.storageService.getCurrentId()).subscribe(
-      data => {
-        this.user = data;
-
-        console.log('Candidate data', data);
-        if (data.rol === 'user1') {
-          this.rol_name = 'Manager';
-        } else if (data.rol === 'user2') {
-          this.rol_name = 'Admin';
-        } else {
-          this.rol_name = 'Colaborator';
-        }
-      });
-  }
-
-  public logout(): void {
-    this.storageService.logout();
-  }
+  ngOnInit() {}
 
   toggle() {
     this.toggled = !this.toggled;
@@ -65,7 +28,7 @@ export class HeaderComponent implements OnInit {
     }
   }
 
-  // function
+  // this function performs the opening of the user's card
   toggleUser() {
    this.show = !this.show;
   }
--- front-end/src/app/user/view-user/card-user/card-user.component.ts
+++ front-end/src/app/user/view-user/card-user/card-user.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
+import { StorageService } from 'src/app/service/storage.service';
+import { AuthenticationService } from 'src/app/service/authentication.service';
+import { UserService } from 'src/app/service/user.service';
+import { User } from 'src/app/models/user';
+
 @Component({
   selector: 'app-card-user',
   templateUrl: './card-user.component.html',
   styleUrls: ['./card-user.component.css']
 })
 export class CardUserComponent implements OnInit {
-
-  constructor() { }
+   /**
+   * Manejo de la Sesión
+   * @param storageService Mantiene la sesión almacenada para que sea persistente
+   * @param authenticationService Para hacer el logout
+   */
+  rol_name: String;
+  user: User = new User();
+  constructor(
+    private storageService: StorageService,
+    private authenticationService: AuthenticationService,
+    private userService: UserService) { }
 
   ngOnInit() {
+    this.userService.getUser(this.storageService.getCurrentId()).subscribe(
+      data => {
+        this.user = data;
+
+        console.log('Candidate data', data);
+        if (data.rol === 'user1') {
+          this.rol_name = 'Manager';
+        } else if (data.rol === 'user2') {
+          this.rol_name = 'Admin';
+        } else {
+          this.rol_name = 'Colaborator';
+        }
+      });
+  }
+
+  public logout(): void {
+    this.storageService.logout();
   }
 
 }
