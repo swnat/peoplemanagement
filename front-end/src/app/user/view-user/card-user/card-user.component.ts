632e9724ee1280a25f1ffdb7a0d46698f4f64532
jose <familiajoselo@gmail.com>
2020-03-17 22:56:58 -0300

    add methods of getting in user profile and role

--- front-end/src/app/user/view-user/card-user/card-user.component.ts
+++ front-end/src/app/user/view-user/card-user/card-user.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { StorageService } from 'src/app/service/storage.service';
 import { AuthenticationService } from 'src/app/service/authentication.service';
 import { UserService } from 'src/app/service/user.service';
 import { User } from 'src/app/models/user';
+import { NotificationService } from 'src/app/shared/notification-service/notification.service';
+
 
 @Component({
   selector: 'app-card-user',
@@ -16,14 +19,33 @@ export class CardUserComponent implements OnInit {
    * @param storageService Mantiene la sesión almacenada para que sea persistente
    * @param authenticationService Para hacer el logout
    */
+
+  // Profile User
+  userProfile: User = {
+    name: '',
+    lastname: '',
+    email: '',
+    active: true,
+    rol: '',
+    phoneNumber: '',
+    idUser: 0,
+};
   rol_name: String;
+
+  rol_name_profile: String; // profile user attribute
   user: User = new User();
   constructor(
     private storageService: StorageService,
     private authenticationService: AuthenticationService,
-    private userService: UserService) { }
+    private userService: UserService,
+    private userServiceProfile: UserService,
+    private notificationService: NotificationService,
+    private route: ActivatedRoute) { }
 
   ngOnInit() {
+    // get rol user
+    this.route.paramMap.subscribe(pmap => this.getUser(pmap.get('id')));
+
     this.userService.getUser(this.storageService.getCurrentId()).subscribe(
       data => {
         this.user = data;
@@ -39,6 +61,41 @@ export class CardUserComponent implements OnInit {
       });
   }
 
+  // get profile user
+  private getUser(id: string): void {
+    if (!id) {
+      this.userProfile = {
+        idUser: 0,
+        name: '',
+        email: '',
+        lastname: '',
+        active: false,
+        phoneNumber: '',
+        rol: ''
+      } as User;
+      this.rol_name_profile = '';
+      return;
+    }
+
+    this.userServiceProfile.getUser(Number(id).valueOf()).subscribe((data: User) => {
+      this.userProfile = data;
+      this.rol_name_profile = this.userRole(data.rol);
+    }, error => {
+      console.log('Error to get data of the user', error);
+      this.notificationService.showError('Occur an error when get data of the user', 'Error get User');
+    });
+  }
+
+  public userRole(rol: String): String {
+    if (rol === 'user1') {
+      return 'Manager';
+    } else if (rol === 'user2') {
+      return 'Admin';
+    }
+    return 'Colaborator';
+  }
+
+  // function logout
   public logout(): void {
     this.storageService.logout();
   }
