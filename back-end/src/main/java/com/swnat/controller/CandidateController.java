69cf671616ef763f2370b980441c701f068a7a5c
Ivan <ivera2065@fpuna.edu.py>
2020-03-30 11:45:49 -0400

    The removeImage and UploadImage methods were removed from that driver

--- back-end/src/main/java/com/swnat/controller/CandidateController.java
+++ back-end/src/main/java/com/swnat/controller/CandidateController.java
@@ -1,5 +1,7 @@
 package com.swnat.controller;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -10,15 +12,6 @@ import io.swagger.annotations.ApiOperation;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.Calendar;
-import java.util.Date;
-import java.nio.file.Files;
 
 
 @RestController
@@ -44,7 +37,7 @@ public class CandidateController {
     public Candidate saveCandidate(@RequestParam("candidate") String candidateForm, @RequestParam(value = "imageProfile", required = false) MultipartFile image) throws JsonParseException, JsonMappingException, IOException {
         Candidate candidate = new ObjectMapper().readValue(candidateForm, Candidate.class);
         if ( image != null){
-            candidate.setProfileImage(uploadImage(image));
+            candidate.setProfileImage(this.candidateService.uploadImage(image));
         }
         return candidateService.add(candidate);
     }
@@ -56,15 +49,15 @@ public class CandidateController {
         Candidate candidate = new ObjectMapper().readValue(candidateForm, Candidate.class);
 
         if ( image == null && candidate.getProfileImage() != null && active.equals("false") ){
-            removeImage(candidate.getProfileImage());
+            this.candidateService.removeImage(candidate.getProfileImage());
             candidate.setProfileImage(null);
         }
         else if ( image != null &&  candidate.getProfileImage() == null ){
-            candidate.setProfileImage(uploadImage(image));
+            candidate.setProfileImage(this.candidateService.uploadImage(image));
         }
         else if ( image != null && candidate.getProfileImage() != null ) {
-            removeImage(candidate.getProfileImage());
-            candidate.setProfileImage(uploadImage(image));
+           this.candidateService.removeImage(candidate.getProfileImage());
+            candidate.setProfileImage(this.candidateService.uploadImage(image));
         }
         
         return candidateService.update(id, candidate);
@@ -74,48 +67,5 @@ public class CandidateController {
     @GetMapping("/{id}")
     public Candidate getCandidate(@PathVariable Long id) {
         return candidateService.getOne(id);
-    }
-
-    public String uploadImage(MultipartFile image) throws IOException {
-        String directory = System.getProperty("user.dir") + File.separator + "src" 
-        + File.separator + "main" + File.separator + "java" + File.separator + "com" + File.separator 
-        + "swnat" + File.separator + "generic_files";
-        Path mypath = Paths.get(directory);
-        if ( !Files.exists(mypath) ){
-            Files.createDirectory(mypath);
-            System.out.println("Directory created");
-        }
-
-        //Formatting the file names to be unique by adding date and time
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(Date.from(Instant.now()));
-        //create filename from a format string
-        String name = String.format("file-%1$tY-%1$tm-%1$td-%1$tk-%1$tS-%1$tp-" + image.getOriginalFilename(), cal);
-        
-        StringBuilder builder = new StringBuilder();
-        builder.append(directory);
-        builder.append(File.separator);
-        builder.append(name);
-
-        byte [] filebytes = image.getBytes();
-        Path path = Paths.get(builder.toString());
-        Files.write(path, filebytes);
-
-        return name;
-    }    
-
-    public void removeImage(String urlImage){
-        String pathImage = System.getProperty("user.dir") + File.separator + "src" 
-        + File.separator + "main" + File.separator + "java" + File.separator + "com" + File.separator 
-        + "swnat" + File.separator + "generic_files" + File.separator + urlImage;
-
-        File fileImage = new File(pathImage);
-        if ( !fileImage.exists()){
-            System.out.println("Image name does not exist");
-        }
-        else{
-            fileImage.delete();
-            System.out.println("Successfully deleted image server");
-        }
     }
 }
