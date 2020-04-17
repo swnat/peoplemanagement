7bd0c7e12ee2d506dd8f72f97f447e87d7588552
Ivan <ivera2065@fpuna.edu.py>
2020-04-17 13:17:02 -0400

    Added a query to find a challenge corresponding to a candidate

--- back-end/src/main/java/com/swnat/repository/ChallengeRepository.java
+++ back-end/src/main/java/com/swnat/repository/ChallengeRepository.java
@@ -1,9 +1,20 @@
 package com.swnat.repository;
 
 import com.swnat.model.Challenge;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
 
-    
+    /**
+     * In the native query it is asked if the candidate has a challenge and retains a value boolean 
+     * @param candidate_id
+     * @return {@link boolean } 
+     */
+
+    @Query(value = "SELECT CASE WHEN (count(*) > 0) THEN true ELSE false END FROM MANAGEMENT.CHALLENGE WHERE CANDIDATE_ID = ?1", 
+    nativeQuery = true)
+    boolean findByChallengeCandidateId(Long candidate_id);
 }
+
