d0fae3ca5e1ac2590e2006641e62cf7548f4e043
joseiba <familiajoselo@gmail.com>
2020-04-19 12:16:02 -0400

    added new test

--- back-end/src/test/java/com/swnat/controller/UploadControllerTest.java
+++ back-end/src/test/java/com/swnat/controller/UploadControllerTest.java
@@ -1,26 +1,22 @@
 package com.swnat.controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
 
-import com.swnat.TestUtils;
-import com.swnat.model.Candidate;
-import com.swnat.model.Challenge;
-import com.swnat.service.CandidateService;
+import java.io.InputStream;
 
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -28,34 +24,45 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import com.swnat.TestUtils;
+import com.swnat.service.ChallengeService;
+
 @RunWith(SpringJUnit4ClassRunner.class) // provide the spring context for the test
 @SpringBootTest
 @AutoConfigureMockMvc//provide configuration for the MockMvc
-public class CandidateControllerTest {
-
+public class UploadControllerTest {	
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvc mockMvc;//provide Spring MVC infrastructure without starting server.
 
     @MockBean
-    private CandidateService candidateService;
-
+    private UploadController uploadcontroller;
+    
     @Autowired
     private WebApplicationContext webApplicationContext;
-
+    
     @Before
     public void setUp() {
+        //lets the Spring know that we want a WebApplicationContext loaded for the project
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
-
+    
     @Test
-    public void getAllCandidateTest() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get(TestUtils.CANDIDATE_URL).
-        param("filter", "S").param("page", "0").param("size", "100").param("sortby", "last modified").
-        contentType(MediaType.APPLICATION_JSON_UTF8).accept(MediaType.APPLICATION_JSON_UTF8)).
-        andReturn();
-
-        // verify status post call
-        int status = result.getResponse().getStatus();
-        assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);
+    public void singleFileUploadTest() throws Exception {
+	
+	  MockMultipartFile multipartFile = new MockMultipartFile("file", "test.jpg",
+		        "image/jpeg", "test image content".getBytes());
+	  
+	  String url = "/api/v1/uploads/upload";
+	  
+	  MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart(url)
+                        .file(multipartFile)
+                        .param("file", "test.png")).andReturn();
+	  
+	  // verify status post call
+	  int status = result.getResponse().getStatus();
+	  assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);
+    	
+    	
     }
-}
+
+} 
--- back-end/src/test/java/com/swnat/controller/UserControllerTest.java
+++ back-end/src/test/java/com/swnat/controller/UserControllerTest.java
@@ -1,9 +1,12 @@
 package com.swnat.controller;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.swnat.TestUtils;
+import com.swnat.model.Challenge;
 import com.swnat.model.User;
 import com.swnat.service.CandidateService;
 import com.swnat.service.UserService;
@@ -11,6 +14,7 @@ import com.swnat.service.UserService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,11 +37,11 @@ public class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private UserController user;
-
     @Mock
-    UserService usermanager;
+    private UserController user;
+    
+    @MockBean
+    private UserService usermanager;
 
     @Autowired
     private WebApplicationContext webApplicationContext;
@@ -55,6 +59,69 @@ public class UserControllerTest {
         andReturn();
         
         int status = result.getResponse().getStatus();
-        //assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);
+        // assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);
+    }
+    @Test
+    public void saveUserTest()throws Exception{
+    	User user = TestUtils.buildUserTest();
+    	when(usermanager.add(any(User.class))).
+    	thenReturn(user);
+    	
+
+        String requestJson= TestUtils.jsonStringFromObject(user);
+
+        // execute
+         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(TestUtils.USER_URL).
+         contentType(MediaType.APPLICATION_JSON_UTF8).
+         accept(MediaType.APPLICATION_JSON_UTF8).
+         content(requestJson)).andReturn();
+
+
+        // verify status post call
+        int status = result.getResponse().getStatus();
+        assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);
+                
+        // verify that service method was called once
+        verify(usermanager).add(any(User.class));
+    }
+    
+    @Test
+    public void updateUserTest() throws Exception{
+    	User user = TestUtils.buildUserTest();
+    	when(usermanager.add(any(User.class))).
+    	thenReturn(user);
+    	
+
+        String requestJson= TestUtils.jsonStringFromObject(user);
+
+        // execute
+         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(TestUtils.USER_URL+"{id]", user.getIdUser()).
+         contentType(MediaType.APPLICATION_JSON_UTF8).
+         accept(MediaType.APPLICATION_JSON_UTF8).
+         content(requestJson)).andReturn();
+
+
+        // verify status post call
+        int status = result.getResponse().getStatus();
+        // assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);
+
+    }
+    
+    @Test
+    public void getUserTest() throws Exception{
+    	User user = TestUtils.buildUserTest();
+    	when(usermanager.add(any(User.class))).
+    	thenReturn(user);
+    	
+        String requestJson= TestUtils.jsonStringFromObject(user);
+
+        // execute
+         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post(TestUtils.USER_URL+"{id}", user.getIdUser()).
+         contentType(MediaType.APPLICATION_JSON_UTF8).
+         accept(MediaType.APPLICATION_JSON_UTF8).
+         content(requestJson)).andReturn();
+         
+         int status = result.getResponse().getStatus();
+         //assertEquals("Incorrect Response Status", HttpStatus.OK.value(), status);     
     }
 }
